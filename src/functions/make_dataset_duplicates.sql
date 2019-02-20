dc3f5abdd63afd834ab2a7c0b87c21be6ca67487
Pavel Petruneac <petruneac.pavel@gmail.com>
2019-02-20 19:36:19 +0000

    update where clause

--- src/functions/make_dataset_duplicates.sql
+++ src/functions/make_dataset_duplicates.sql
@@ -1,7 +1,6 @@
--- This section will read ALL the data from MySQL, apart from new resources (latest data)
+-- This section will read ALL the data from MySQL, apart from new resources (latest data, presumably last day)
 -- where you specify in WHERE clause.
 -- file will be saved in sample/data/dataset_duplicates_all.csv
-
 use resurse_crestine;
 
 SELECT id, title, type, content, language
@@ -12,14 +11,12 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
 
 
--- This section will read a sample of the data from MySQL.
+-- This section will read a sample of the data from MySQL (i.e. last day).
 -- file will be saved in sample/data/dataset_duplicates_latest.csv
-
 use resurse_crestine;
 
 SELECT id, title, type, content, language
-FROM resurse 
-ORDER BY RAND() LIMIT 10
+FROM resurse -- WHERE day = last day
 INTO OUTFILE '/tmp/dataset_duplicates_latest.csv'
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'

b61ff302ae27a6952c7001bfdecca68039d22b02
Pavel <petruneac.pavel@gmail.com>
2019-02-20 18:50:37 +0000

    updates

--- src/functions/create_mysql_db_test_mode.sql
+++ src/functions/create_mysql_db_test_mode.sql
@@ -1,9 +1,10 @@
 
--- This script creates a db and writes a sample table;
 
 SET GLOBAL local_infile = 1;
 set global max_allowed_packet = 1000000;
 
+-- create resurse_crestine db
+drop database resurse_crestine;
 CREATE DATABASE resurse_crestine;
 use resurse_crestine;
 
@@ -12,17 +13,27 @@ CREATE TABLE resurse (id INT NOT NULL,
   type INT NOT NULL, 
   content TEXT,
   datetime DATETIME,  
+  language VARCHAR(100),
   PRIMARY KEY (id));
 
-LOAD DATA LOCAL INFILE '../data/sample/tilda_separated_filter_clean.csv' 
+LOAD DATA LOCAL INFILE '../data/sample/sample_data_lan.csv' 
 INTO TABLE resurse_crestine.resurse
   FIELDS TERMINATED BY ','
   LINES TERMINATED BY '\n'
   IGNORE 1 ROWS;
 
+-- create duplicates db
+drop database duplicates;
+CREATE DATABASE duplicates;
+use duplicates;
 
-
-
+CREATE TABLE duplicates (id INT NOT NULL, 
+  id_dup INT NOT NULL, 
+  title VARCHAR(500) NOT NULL, 
+  title_dup VARCHAR(500) NOT NULL, 
+  cos_similarity NUMERIC NOT NULL,
+  type INT NOT NULL,
+  PRIMARY KEY (id));
 
 
 
--- src/functions/load_duplicates_to_MySQL.sql
+++ src/functions/load_duplicates_to_MySQL.sql
@@ -1,6 +1,9 @@
---
+
 use duplicates;
 LOAD DATA LOCAL INFILE '../data/interim/duplicates_latest.csv' 
 INTO TABLE duplicates.duplicates
   FIELDS TERMINATED BY ','
-  LINES TERMINATED BY '\n';
+  LINES TERMINATED BY '\n'
+  IGNORE 1 ROWS;;
+
+-- select * from duplicates.duplicates;

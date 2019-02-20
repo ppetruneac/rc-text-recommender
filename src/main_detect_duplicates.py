07961ef95f34f94e3c31ac2ff9a0e5637f4b6fc3
Pavel <petruneac.pavel@gmail.com>
2019-02-20 18:58:00 +0000

    working version to detect duplicates

--- src/main_detect_duplicates.py
+++ src/main_detect_duplicates.py
@@ -6,70 +6,61 @@ from functions import get_term_frequency_voc
 from functions import detect_duplicates
 import os
 
-# If reading the data from real MySQL db, change to False. If True, will upload sample data into db
-# for demo purpose. 
-test_mode = False
+ 
+test_mode = True
+# test_mode (boolean); if set to True, will:
+#   1. create database in MySQL with sample data (table) 
+#   2. Create duplicates db with empty table.
 
-# Indicate if this is the first run. This will create: ../data/interim/tf_voc folder and create a db
-# in MySQL for duplicates.
-#  Change to `False` if not the first run.
-first_run = False
-sudo_password = 'pavdanawed2018'
+first_run = True
+# first_run : boolean. If True it will create data/interim/tf_voc folder
+
+sudo_password = 'sudo_password'
 
 if test_mode:
-  # Create a sample db with sample data.  
-  print('Creating a sample db (resurse_crestine) in MySQL ... ')
-  bashCommand = "echo " + sudo_password + "  | sudo -S mysql --user=root  < functions/create_mysql_db_test_mode.sql"
+  # Create a db with sample data.  
+  print('\nCreating a sample db (resurse_crestine) in MySQL --> TEST_MODE ... ')
+  bashCommand = "echo " + sudo_password + "  | sudo mysql --user=root  < functions/create_mysql_db_test_mode.sql"
   os.system(bashCommand) 
 
 
-print('\nReading ALL data from MySQL apart from new resources (latest data)... ')
-# ===============CHANGE__THIS__SQL__FILE===============
-# bashCommand = "echo " + sudo_password + "  | sudo -S mysql --user=root  < functions/make_dataset_dup_all_data.sql"
-# os.system(bashCommand)
-
-# # Move the file from /tmp folder
-# bashCommand = "echo " + sudo_password + "| sudo mv /tmp/dataset_duplicates_all.csv ../data/sample/"
-# os.system(bashCommand)
-# df = pd.read_csv('../data/sample/dataset_duplicates_all.csv')
-# df.columns = ['id', 'title', 'type', 'content', 'datetime']
-# df = df[['id', 'title', 'type', 'content']]
-df_ref = pd.read_csv('../data/sample/tilda_separated.csv', delimiter='~')
-print("\tshape = {}".format(df_ref.shape))
-
-print("Reading new resources (latest data) from MySQL ... ")
-bashCommand = "echo " + sudo_password + "  | sudo -S mysql --user=root  < functions/make_dataset_dup_latest_data.sql"
+print('\nReading data from MySQL ... ')
+# ===============CHANGE__THIS__SQL__FILE: make_dataset_duplicates.sql ===============
+bashCommand = "echo " + sudo_password + "  | sudo mysql --user=root  < functions/make_dataset_duplicates.sql"
 os.system(bashCommand)
-# Move the file from /tmp folder
+
+# Move the files from /tmp folder
+bashCommand = "echo " + sudo_password + "| sudo mv /tmp/dataset_duplicates_all.csv ../data/sample/"
+os.system(bashCommand)
 bashCommand = "echo " + sudo_password + "| sudo mv /tmp/dataset_duplicates_latest.csv ../data/sample/"
 os.system(bashCommand)
+
+# Importing data in Python env
+df_ref = pd.read_csv('../data/sample/dataset_duplicates_all.csv')
+df_ref.columns = ['id', 'title', 'type', 'content', 'language']
+print("\tshape df_ref = {}".format(df_ref.shape))
+
 df_latest = pd.read_csv('../data/sample/dataset_duplicates_latest.csv')
-df_latest.columns = ['id', 'title', 'type', 'content', 'datetime']
-df_latest = df_latest[['id', 'title', 'type', 'content']]
-print("\tshape = {}".format(df_latest.shape))
+df_latest.columns = ['id', 'title', 'type', 'content', 'language']
+print("\tshape df_latest = {}".format(df_latest.shape))
+
+df_ref = df_ref[~df_ref['id'].isin(df_latest['id'])]
 
 # Cleaning the data
 df_ref = clean_data.clean_data(df_ref, verbose = True, write = False, save_file_path = "../")
 df_latest = clean_data.clean_data(df_latest, verbose = True, write = False, save_file_path = "../")
 
-# Only for testing mode -- DELETE when read from actual MySQL.
-# This will remove the latest resources (id level) from the all data. 
-df_ref = df_ref[~df_ref['id'].isin(df_latest.id)]
 
 # Creating the tf vocabularily and save it to data/interim/tf
 if first_run:
-  df_combined = pd.concat([df_ref, df_latest], axis = 1)
+  df_combined = pd.concat([df_ref, df_latest], axis = 0)
   get_term_frequency_voc.get_term_frequency_voc(df_combined, verbose = True, save_file_path = "../")
-  
-  # Create duplicates db
-  bashCommand = "echo " + sudo_password + "  | sudo -S mysql --user=root  < functions/create_duplicates_db.sql"
-  os.system(bashCommand)
  
 #  Detect duplicates for new resources
 duplicates = detect_duplicates.detect_duplicates(df_ref, df_latest, verbose=True, write = True, save_file_path = "../")
 
 # Load new duplicates to MySQL
 print('Loading the latest duplicates to MySQL ...')
-bashCommand = "echo " + sudo_password + "  | sudo -S mysql --user=root  < functions/load_duplicates_to_MySQL.sql"
+bashCommand = "echo " + sudo_password + "  | sudo mysql --user=root  < functions/load_duplicates_to_MySQL.sql"
 os.system(bashCommand)
 print('DONE! ')

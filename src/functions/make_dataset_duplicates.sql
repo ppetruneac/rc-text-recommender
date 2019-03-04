ceb4278469c270adb85be87878499b13a60327a1
D DS Team <luis.vaquero@dyson.com>
2019-03-04 20:19:29 +0000

    updates to reflect new tables structure

--- src/functions/make_dataset_duplicates.sql
+++ src/functions/make_dataset_duplicates.sql
@@ -1,10 +1,20 @@
 -- This section will read ALL the data from MySQL, apart from new resources (latest data, presumably last day)
 -- where you specify in WHERE clause.
--- file will be saved in sample/data/dataset_duplicates_all.csv
-use resurse_crestine;
+-- file will be saved in the /tmp/, then moved to sample/data/dataset_duplicates_all.csv
+use resursenew;
 
-SELECT id, title, type, content, language
-FROM resurse -- WHERE date not in last day 
+SELECT
+  resources.id,
+  resources.res_type_id,
+  resources.language_id,
+  resources.title,
+  resources.created_at,
+  text_details.content  
+FROM
+  resources
+LEFT JOIN text_details ON resources.id = text_details.resources_id
+WHERE
+  text_details.content is not null and resources.created_at < (NOW() - INTERVAL 300 HOUR) -- resources not in the last 24h.  
 INTO OUTFILE '/tmp/dataset_duplicates_all.csv'
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -12,11 +22,21 @@ LINES TERMINATED BY '\n';
 
 
 -- This section will read a sample of the data from MySQL (i.e. last day).
--- file will be saved in sample/data/dataset_duplicates_latest.csv
-use resurse_crestine;
+-- file will be saved in the /tmp/, then moved to sample/data/dataset_duplicates_latest.csv
+use resursenew;
 
-SELECT id, title, type, content, language
-FROM resurse -- WHERE day = last day
+SELECT
+  resources.id,
+  resources.res_type_id,
+  resources.language_id,
+  resources.title,
+  resources.created_at,
+  text_details.content  
+FROM
+  resources
+LEFT JOIN text_details ON resources.id = text_details.resources_id
+WHERE
+  text_details.content is not null and resources.created_at > (NOW() - INTERVAL 300 HOUR) -- resources not in the last 24h.  
 INTO OUTFILE '/tmp/dataset_duplicates_latest.csv'
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'

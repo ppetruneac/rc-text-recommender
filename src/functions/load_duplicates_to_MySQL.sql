c56ed43e209f88256b716488c4a53734a1f13ab9
Pavel <petruneac.pavel@gmail.com>
2019-03-14 20:13:36 +0000

    changed table to read from

--- src/functions/load_duplicates_to_MySQL.sql
+++ src/functions/load_duplicates_to_MySQL.sql
@@ -1,7 +1,7 @@
 
 use duplicates;
 LOAD DATA LOCAL INFILE '../data/interim/duplicates_latest.csv' 
-INTO TABLE duplicates.duplicates
+INTO TABLE duplicates.all_duplicates
   FIELDS TERMINATED BY ','
   LINES TERMINATED BY '\n'
   IGNORE 1 ROWS;

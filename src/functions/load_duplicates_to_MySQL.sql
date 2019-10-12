86d07d5237e6fa9ef481661e5900e402fb73f678
Pavel <petruneac.pavel@gmail.com>
2019-10-12 16:59:27 +0100

    rm files

--- src/functions/load_duplicates_to_MySQL.sql
+++
@@ -1,6 +0,0 @@
-use duplicates;
-LOAD DATA LOCAL INFILE '../data/interim/duplicates_latest.csv' 
-INTO TABLE duplicates.all_duplicates
-  FIELDS TERMINATED BY ','
-  LINES TERMINATED BY '\n'
-  IGNORE 1 ROWS;

1ad4e9a03422e02a8a5b4f47c592c1b9957f526d
Pavel Petruneac <petruneac.pavel@gmail.com>
2019-11-04 21:39:09 +0000

    Update requirements.sh

--- detect_duplicates/requirements.sh
+++ detect_duplicates/requirements.sh
@@ -2,7 +2,7 @@
 sudo yum -y update
 # sudo yum -y install yum-utils
 sudo yum -y groupinstall developments
-sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+sudo yum -y install https://centos6.iuscommunity.org/ius-release.rpm
 sudo yum install -y python36u python36u-libs python36u-devel python36u-pip
 pip install --upgrade pip
 sudo yum -y install python36u-devel
